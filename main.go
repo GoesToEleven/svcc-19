0c681a43ed0269f5863427df1d7810948154c8b5
Todd McLeod <tuddleymc@gmail.com>
2019-10-19 13:39:54 -0700

    composition

--- main.go
+++ main.go
@@ -4,7 +4,13 @@ import "fmt"
 
 type person struct {
 	first string
-	last string
+	last  string
+}
+
+// func (r receiver) identifier(parameters) return(s) { code }
+
+func (p person) speak() {
+	fmt.Println("I AM A PERSON NAMED", p.first, p.last)
 }
 
 type secretAgent struct {
@@ -12,9 +18,41 @@ type secretAgent struct {
 	ltk bool
 }
 
+func (sa secretAgent) speak() {
+	fmt.Println("I AM A PERSON NAMED", sa.first, sa.last)
+}
+
+type human interface {
+	speak()
+}
+
+func foo(h human) {
+	h.speak()
+}
+
 func main() {
-	fname := "Todd McLeod"
-	fmt.Println("Hello world. I am", fname)
-	fmt.Printf("%T\n", y)
-	fmt.Println(y)
+	// composite literal
+	// p1 := type{values}
+	p1 := person{
+		first: "Jenny",
+		last:  "Moneypenny",
+	}
+	fmt.Println(p1)
+
+	p2 := secretAgent{
+		person: person{
+			first: "James",
+			last:  "Bond",
+		},
+		ltk: true,
+	}
+	fmt.Println(p2)
+
+	foo(p1)
+	foo(p2)
+
+	/*
+	in go we really talk about and think about
+	a VALUE being of certain TYPES
+	*/
 }
