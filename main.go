608f335c513a3d9af62d4dd26bc1e271a616ffd1
Todd McLeod <tuddleymc@gmail.com>
2019-10-19 13:58:04 -0700

    end

--- main.go
+++ main.go
@@ -4,16 +4,11 @@ import "fmt"
 
 func main() {
 	x := 42
-	fmt.Println(x)
-	fmt.Println(&x)
-	fmt.Println("-----")
-	fmt.Printf("%T\n", x)
-	fmt.Printf("%T\n", &x)
-	fmt.Println("-----")
-	fmt.Println(*&x)
+	foo(&x)
+	fmt.Println("back in main", x)
 }
 
-func foo(n int) {
-	n = 43
+func foo(n *int) {
+	*n = 43
 	fmt.Println("inside foo", n)
 }
